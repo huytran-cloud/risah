ff2275a63d6a226d13e800d8bab6e9d2461a216e
huythanthanh <trum.huy1036@gmail.com>
2022-07-22 01:46:14 +0700

    70% done homepage

--- js/main.js
+++ js/main.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
 
+    // Owl-Carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 6969,
+        // nav: true,
+        // navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
+
 })
